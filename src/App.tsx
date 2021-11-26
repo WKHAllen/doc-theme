6dae3c97ac90d48749248f3e61fd7479487c59d8
WKHAllen <wkhallen@gmail.com>
2021-11-26 18:46:24 -0500

    Begin app component implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import React from "react";
 import "./App.css";
+import { document as doc } from "./document.json";
 
-export default class App extends React.Component {
-  render() {
-    return <div className="App">Doc Theme</div>;
+export interface DocumentComponent {}
+
+interface Document {
+  name: string;
+  theme: string;
+  content: DocumentComponent[];
+}
+
+interface AppState {
+  doc: Document;
+}
+
+export default class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      doc,
+    };
+  }
+
+  public componentDidMount() {
+    document.title = this.state.doc.name;
+  }
+
+  public render() {
+    return <></>;
   }
 }
